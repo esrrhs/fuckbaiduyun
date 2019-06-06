2e079c08addfb06de7c51455c7556e91e4f55e75
xin zhao <zhaoxin29@baidu.com>
2019-06-06 16:13:12 +0800

    update

--- alg.go
+++ alg.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"crypto/rc4"
+	"encoding/hex"
 	"fmt"
+	"github.com/esrrhs/go-engine/src/loggo"
+	"github.com/esrrhs/go-engine/src/rbuffergo"
+	"io"
 	"io/ioutil"
-	"log"
+	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -46,48 +53,45 @@ func getallfiles(pathname string, s []string) ([]string, error) {
 	return s, nil
 }
 
-var workResultLock sync.WaitGroup
-var num int32
-var jobtotal int32
-var jobdone int32
+func showError(err error) {
+	loggo.Error("%v", err)
+	panic(err.Error())
+}
 
-func main1() {
-	log.Print("start")
+func showErrorStr(err string) {
+	loggo.Error("%v", err)
+	panic(err)
+}
+
+func dojob(jobtotal *int32, jobdone *int32, input string, output string, doen bool,
+	key string, split int) {
+
+	var done map[string]int
+	done = make(map[string]int)
+	var workResultLock sync.WaitGroup
+
+	loggo.Ini(loggo.Config{loggo.LEVEL_DEBUG, "fuck", 2})
+
+	loggo.Info("start")
+
+	if strings.Contains(output, input) || strings.Contains(input, output) {
+		showErrorStr("输入输出目录有重叠")
+		return
+	}
 
 	var s []string
 
-	s, err := getallfiles("./", s)
+	loggo.Info("get all file begin %v", input)
+
+	s, err := getallfiles(input+"/", s)
 	if err != nil {
-		log.Fatal(err)
-		return
+		showError(err)
 	}
 
-	log.Print("get all file done ", len(s))
+	loggo.Info("get all file done %v", len(s))
 
-	for _, ss := range s {
-
-		if strings.HasPrefix(filepath.Base(ss), "fuckbaiduyun") {
-			continue
-		}
-
-		if strings.HasSuffix(filepath.Base(ss), "fuckbaiduyun") {
-			if exists(strings.TrimSuffix(ss, ".fuckbaiduyun")) {
-				err = os.Remove(ss)
-				if err != nil {
-					log.Fatal(err)
-					return
-				}
-			}
-		}
-	}
-
-	var s1 []string
-	s, err = getallfiles("./", s1)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	log.Print("fix all file done ", len(s))
+	loadDone(output, done)
+	loggo.Info("loadDone %v", len(done))
 
 	total := 0
 	for _, ss := range s {
@@ -99,28 +103,29 @@ func main1() {
 		total++
 	}
 
-	en := 0
-	for _, ss := range s {
-		if strings.HasSuffix(filepath.Base(ss), "fuckbaiduyun") {
-			en++
-		}
-	}
+	loggo.Info("all file total %v", total)
 
-	doen := false
-	if en > total/2 {
-		doen = true
-	}
-	if en == total {
-		doen = false
-	}
-	if en == 0 {
-		doen = true
-	}
+	var num int32
 
 	num = 0
 
-	jobdone = 0
-	jobtotal = 0
+	for _, ss := range s {
+
+		if strings.HasPrefix(filepath.Base(ss), "fuckbaiduyun") {
+			continue
+		}
+		if strings.HasSuffix(filepath.Base(ss), "fuckbaiduyun") {
+			if !doen {
+				*jobtotal++
+			}
+		} else {
+			if doen {
+				*jobtotal++
+			}
+		}
+	}
+
+	loggo.Info("all job file jobtotal %v", jobtotal)
 
 	for _, ss := range s {
 
@@ -129,101 +134,141 @@ func main1() {
 		}
 		if strings.HasSuffix(filepath.Base(ss), "fuckbaiduyun") {
 			if !doen {
-				jobtotal++
+				defuck(workResultLock, &num, key, split, ss, false,
+					jobdone, jobtotal, done, input, output)
 			}
 		} else {
 			if doen {
-				jobtotal++
+				fuck(workResultLock, &num, key, split, ss, false,
+					jobdone, jobtotal, done, input, output)
 			}
 		}
 	}
 
-	for _, ss := range s {
-
-		if strings.HasPrefix(filepath.Base(ss), "fuckbaiduyun") {
-			continue
-		}
-		if strings.HasSuffix(filepath.Base(ss), "fuckbaiduyun") {
-			if !doen {
-				if num < 100 {
-					atomic.AddInt32(&num, 1)
-					workResultLock.Add(1)
-					go defuck(ss, true)
-				} else {
-					defuck(ss, false)
-				}
-			}
-		} else {
-			if doen {
-				if num < 100 {
-					atomic.AddInt32(&num, 1)
-					workResultLock.Add(1)
-					go fuck(ss, true)
-				} else {
-					fuck(ss, false)
-				}
-			}
-		}
-	}
 	workResultLock.Wait()
+	delDone(output)
 }
 
-func defuck(ss string, flag bool) {
-	log.Print("start back : ", ss)
+func defuck(workResultLock sync.WaitGroup, num *int32, key string, split int, ss string, flag bool,
+	jobdone *int32, jobtotal *int32, done map[string]int, input string, output string) {
+
+	loggo.Info("start back : %v", ss)
 
 	if flag {
 		defer workResultLock.Done()
-		defer atomic.AddInt32(&num, -1)
+		defer atomic.AddInt32(num, -1)
+	}
+
+	if done[ss] == 1 {
+		atomic.AddInt32(jobdone, 1)
+		loggo.Info("end back skip done : %v/%v %v", *jobdone, *jobtotal, ss)
+		return
+	}
+
+	outputss := strings.Replace(strings.TrimSuffix(ss, ".fuckbaiduyun"), input, output, -1)
+	folderPath := filepath.Dir(outputss)
+	os.MkdirAll(folderPath, os.ModePerm)
+
+	var son []string
+
+	rd, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		showError(err)
+	}
+	for _, fi := range rd {
+		if !fi.IsDir() {
+			m, _ := filepath.Match("*.fuckbaiduyun.*", fi.Name())
+			if m {
+				loggo.Info("back add split: %v %v", ss, fi.Name())
+				son = append(son, folderPath+"/"+fi.Name())
+			}
+		}
 	}
 
 	ifile, err := os.Open(ss)
 	if err != nil {
-		log.Fatal(err)
-		return
+		showError(err)
 	}
 
 	// Open file for writing
-	ofile, err := os.OpenFile(strings.TrimSuffix(ss, ".fuckbaiduyun"), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
+	ofile, err := os.OpenFile(outputss, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
 	if err != nil {
-		log.Fatal(err)
-		ifile.Close()
-		return
+		showError(err)
 	}
 
 	bufferedReader := bufio.NewReader(ifile)
 
 	bufferedWriter := bufio.NewWriter(ofile)
 
-	for i := 0; i < len("fuckbaiduyun"); i++ {
-		bufferedReader.ReadByte()
-	}
+	byteSlice := make([]byte, 4*1024*1024)
 
-	byteSlice := make([]byte, 1024*1024)
+	rb := rbuffergo.New(16*1024*1024, false)
 
-	for {
-		numBytesRead, err := bufferedReader.Read(byteSlice)
+	var post int
 
-		if numBytesRead == 0 {
-			break
+	fileend := false
+	for !fileend || !rb.Empty() {
+		for !fileend && rb.Size()+len(byteSlice) < rb.Capacity() {
+
+			numBytesRead, err := bufferedReader.Read(byteSlice)
+
+			if numBytesRead == 0 {
+				find := false
+
+				for _, sf := range son {
+					if sf == ss+"."+strconv.Itoa(post) {
+						find = true
+					}
+				}
+
+				if find {
+					ifile.Close()
+					ifile, err = os.Open(ss + "." + strconv.Itoa(post))
+					if err != nil {
+						showError(err)
+					}
+					post++
+					bufferedReader = bufio.NewReader(ifile)
+					continue
+				} else {
+					fileend = true
+					break
+				}
+			}
+
+			if err != nil {
+				showError(err)
+			}
+
+			rb.Write(byteSlice[:numBytesRead])
 		}
 
-		if err != nil {
-			log.Fatal(err)
-			ifile.Close()
-			ofile.Close()
-			return
-		}
+		for !rb.Empty() {
+			if rb.Size() < 1024*1024 {
+				if !fileend {
+					break
+				}
+			}
 
-		numBytesWrite, err := bufferedWriter.Write(byteSlice[:numBytesRead])
-		if err != nil || numBytesRead != numBytesWrite {
-			log.Fatal(err)
-			log.Fatal(numBytesRead, numBytesWrite)
-			ifile.Close()
-			ofile.Close()
-			return
-		}
+			numBytesRead := int(math.Min(float64(rb.Size()), 1024*1024))
 
-		bufferedWriter.Flush()
+			if !rb.Read(byteSlice[0:numBytesRead]) {
+				showErrorStr("rbuffergo read fail " + ss)
+			}
+
+			d := decrypt(byteSlice[:numBytesRead], key)
+			numBytesRead = len(d)
+
+			numBytesWrite, err := bufferedWriter.Write(d)
+			if err != nil {
+				showError(err)
+			}
+			if numBytesRead != numBytesWrite {
+				showErrorStr("diff size " + strconv.Itoa(numBytesRead) + " " + strconv.Itoa(numBytesWrite))
+			}
+
+			bufferedWriter.Flush()
+		}
 	}
 
 	ifile.Close()
@@ -231,82 +276,206 @@ func defuck(ss string, flag bool) {
 
 	err = os.Remove(ss)
 	if err != nil {
-		log.Fatal(err)
-		return
+		showError(err)
 	}
 
-	atomic.AddInt32(&jobdone, 1)
+	atomic.AddInt32(jobdone, 1)
 
-	log.Print("end back : ", jobdone, "/", jobtotal, " ", ss)
+	done[ss] = 1
+	saveDone(output)
+
+	loggo.Info("end back : %v/%v %v", *jobdone, *jobtotal, ss)
 }
 
-func fuck(ss string, flag bool) {
-	log.Print("start fuck : ", ss)
+func saveDone(output string) {
+
+	file, err := os.Open(output + "/fuckbaiduyunDONE")
+	if err != nil {
+		file, err = os.OpenFile(output+"/fuckbaiduyunDONE", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			showError(err)
+		}
+	}
+
+	wd := bufio.NewWriter(file)
+	wd.WriteString(output + "\n")
+
+	file.Close()
+}
+
+func delDone(output string) {
+	os.Remove(output + "/fuckbaiduyunDONE")
+}
+
+func loadDone(output string, done map[string]int) {
+
+	os.MkdirAll(output, os.ModePerm)
+
+	file, err := os.Open(output + "/fuckbaiduyunDONE")
+	if err != nil {
+		file, err = os.OpenFile(output+"/fuckbaiduyunDONE", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			showError(err)
+		}
+	}
+
+	rd := bufio.NewReader(file)
+	for {
+		line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
+
+		if err != nil || io.EOF == err {
+			break
+		}
+
+		done[line] = 1
+	}
+
+	file.Close()
+}
+
+func fuck(workResultLock sync.WaitGroup, num *int32, key string, split int, ss string, flag bool,
+	jobdone *int32, jobtotal *int32, done map[string]int, input string, output string) {
+
+	loggo.Info("start fuck : %v", ss)
 
 	if flag {
 		defer workResultLock.Done()
-		defer atomic.AddInt32(&num, -1)
+		defer atomic.AddInt32(num, -1)
+	}
+
+	if done[ss] == 1 {
+		atomic.AddInt32(jobdone, 1)
+		loggo.Info("end fuck skip done : %v/%v %v", *jobdone, *jobtotal, ss)
+		return
+	}
+
+	m, _ := filepath.Match("*.fuckbaiduyun.*", filepath.Base(ss))
+	if m {
+		atomic.AddInt32(jobdone, 1)
+		loggo.Info("end fuck skip split: %v/%v %v", *jobdone, *jobtotal, ss)
+		return
 	}
 
 	ifile, err := os.Open(ss)
 	if err != nil {
-		log.Fatal(err)
-		return
+		showError(err)
 	}
 
+	outputss := strings.Replace(ss, input, output, -1)
+	folderPath := filepath.Dir(outputss)
+	os.MkdirAll(folderPath, os.ModePerm)
+
 	// Open file for writing
-	ofile, err := os.OpenFile(ss+".fuckbaiduyun", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	ofile, err := os.OpenFile(outputss+".fuckbaiduyun",
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
-		log.Fatal(err)
-		ifile.Close()
-		return
+		showError(err)
 	}
 
 	bufferedReader := bufio.NewReader(ifile)
 
 	bufferedWriter := bufio.NewWriter(ofile)
 
-	bufferedWriter.WriteString("fuckbaiduyun")
-	bufferedWriter.Flush()
+	byteSlice := make([]byte, 4*1024*1024)
 
-	byteSlice := make([]byte, 1024*1024)
+	rb := rbuffergo.New(16*1024*1024, false)
 
-	for {
-		numBytesRead, err := bufferedReader.Read(byteSlice)
+	var cur int
+	var post int
 
-		if numBytesRead == 0 {
-			break
+	fileend := false
+	for !fileend || !rb.Empty() {
+		for !fileend && rb.Size()+len(byteSlice) < rb.Capacity() {
+
+			numBytesRead, err := bufferedReader.Read(byteSlice)
+
+			if numBytesRead == 0 {
+				fileend = true
+				break
+			}
+
+			if err != nil {
+				showError(err)
+			}
+
+			rb.Write(byteSlice[:numBytesRead])
 		}
 
-		if err != nil {
-			log.Fatal(err)
-			ifile.Close()
-			ofile.Close()
-			return
-		}
+		for !rb.Empty() {
+			if rb.Size() < 1024*1024 {
+				if !fileend {
+					break
+				}
+			}
 
-		numBytesWrite, err := bufferedWriter.Write(byteSlice[:numBytesRead])
-		if err != nil || numBytesRead != numBytesWrite {
-			log.Fatal(err)
-			log.Fatal(numBytesRead, numBytesWrite)
-			ifile.Close()
-			ofile.Close()
-			return
-		}
+			numBytesRead := int(math.Min(float64(rb.Size()), 1024*1024))
 
-		bufferedWriter.Flush()
+			if !rb.Read(byteSlice[0:numBytesRead]) {
+				showErrorStr("rbuffergo read fail " + ss)
+			}
+
+			d := encrypt(byteSlice[:numBytesRead], key)
+
+			cur += numBytesRead
+
+			if cur > split {
+				ofile.Close()
+				bufferedWriter.Flush()
+				ofile, err = os.OpenFile(outputss+".fuckbaiduyun"+"."+strconv.Itoa(post),
+					os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+				if err != nil {
+					showError(err)
+				}
+				bufferedWriter = bufio.NewWriter(ofile)
+				post++
+				cur -= split
+			}
+
+			numBytesWrite, err := bufferedWriter.Write(d)
+			if err != nil {
+				showError(err)
+			}
+			if numBytesRead != numBytesWrite {
+				showErrorStr("diff size " + strconv.Itoa(numBytesRead) + " " + strconv.Itoa(numBytesWrite))
+			}
+
+			bufferedWriter.Flush()
+		}
 	}
 
 	ifile.Close()
 	ofile.Close()
 
-	err = os.Remove(ss)
+	atomic.AddInt32(jobdone, 1)
+
+	done[ss] = 1
+	saveDone(output)
+
+	loggo.Info("end fuck : %v/%v %v", *jobdone, *jobtotal, ss)
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	c, err := rc4.NewCipher([]byte(createHash(passphrase)))
 	if err != nil {
-		log.Fatal(err)
-		return
+		showError(err)
 	}
+	dst := make([]byte, len(data))
+	c.XORKeyStream(dst, data)
+	return data
+}
 
-	atomic.AddInt32(&jobdone, 1)
-
-	log.Print("end fuck : ", jobdone, "/", jobtotal, " ", ss)
+func decrypt(data []byte, passphrase string) []byte {
+	c, err := rc4.NewCipher([]byte(createHash(passphrase)))
+	if err != nil {
+		showError(err)
+	}
+	dst := make([]byte, len(data))
+	c.XORKeyStream(dst, data)
+	return data
 }
--- main.go
+++ main.go
@@ -1,13 +1,11 @@
-//source: http://doc.qt.io/qt-5/qtwidgets-widgets-lineedits-example.html
-
 package main
 
 import (
-	"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/gui"
+	"fmt"
 	"github.com/therecipe/qt/widgets"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
@@ -18,11 +16,11 @@ func main() {
 
 	echoGroup := widgets.NewQGroupBox2("", nil)
 
-	inputLabel := widgets.NewQLabel2("输入", nil, 0)
+	inputLabel := widgets.NewQLabel2("输入：", nil, 0)
 	input := widgets.NewQLineEdit(nil)
 	input.SetText(filepath.Dir(os.Args[0]))
 
-	outputLabel := widgets.NewQLabel2("输出", nil, 0)
+	outputLabel := widgets.NewQLabel2("输出：", nil, 0)
 	output := widgets.NewQLineEdit(nil)
 
 	inputButton := widgets.NewQPushButton2("选择", nil)
@@ -34,9 +32,20 @@ func main() {
 		input.SetText(w.GetExistingDirectory(nil, "选择输入目录", "", 0))
 	})
 	outputButton.ConnectClicked(func(checked bool) {
-		w := widgets.NewQFileDialog2(nil, "选择输入目录", "", "")
+		w := widgets.NewQFileDialog2(nil, "选择输出目录", "", "")
 		w.SetFileMode(widgets.QFileDialog__DirectoryOnly)
-		output.SetText(w.GetExistingDirectory(nil, "选择输入目录", "", 0))
+		output.SetText(w.GetExistingDirectory(nil, "选择输出目录", "", 0))
+	})
+
+	passLabel := widgets.NewQLabel2("密码：", nil, 0)
+	pass := widgets.NewQLineEdit(nil)
+	pass.SetText("123456")
+
+	swapButton := widgets.NewQPushButton2("交换", nil)
+	swapButton.ConnectClicked(func(checked bool) {
+		tmp := input.Text()
+		input.SetText(output.Text())
+		output.SetText(tmp)
 	})
 
 	split := widgets.NewQComboBox(nil)
@@ -46,7 +55,32 @@ func main() {
 	fuckButton := widgets.NewQPushButton2("GO", nil)
 
 	cur := widgets.NewQProgressBar(nil)
-	cur.SetValue(80)
+
+	fuckButton.ConnectClicked(func(checked bool) {
+		defer func() {
+			if err := recover(); err != nil {
+				a := widgets.NewQMessageBox(nil)
+				a.SetText(fmt.Sprintf("%v", err))
+				a.Show()
+				//os.Exit(1)
+			}
+		}()
+
+		cur.SetValue(0)
+
+		var jobtotal int32
+		var jobdone int32
+
+		dojob(&jobtotal, &jobdone, input.Text(), output.Text(), do.CurrentText() == "加密", pass.Text(),
+			1024*1024)
+
+		for jobtotal > jobdone {
+			cur.SetValue(int(jobdone * 100 / jobtotal))
+			time.Sleep(time.Duration(100) * time.Millisecond)
+		}
+
+		cur.SetValue(100)
+	})
 
 	var echoLayout = widgets.NewQGridLayout2()
 	echoLayout.AddWidget(inputLabel, 0, 0, 0)
@@ -55,10 +89,15 @@ func main() {
 	echoLayout.AddWidget(outputLabel, 1, 0, 0)
 	echoLayout.AddWidget(output, 1, 1, 0)
 	echoLayout.AddWidget(outputButton, 1, 2, 0)
-	echoLayout.AddWidget(split, 2, 0, 0)
-	echoLayout.AddWidget(do, 2, 1, 0)
-	echoLayout.AddWidget(fuckButton, 2, 2, 0)
-	echoLayout.AddWidget3(cur, 3, 0, 3, 3, 0)
+
+	echoLayout.AddWidget(passLabel, 2, 0, 0)
+	echoLayout.AddWidget(pass, 2, 1, 0)
+	echoLayout.AddWidget(swapButton, 2, 2, 0)
+
+	echoLayout.AddWidget(split, 3, 0, 0)
+	echoLayout.AddWidget(do, 3, 1, 0)
+	echoLayout.AddWidget(fuckButton, 3, 2, 0)
+	echoLayout.AddWidget3(cur, 4, 0, 3, 3, 0)
 	echoGroup.SetLayout(echoLayout)
 
 	var layout = widgets.NewQGridLayout2()
@@ -71,108 +110,4 @@ func main() {
 	window.Show()
 
 	widgets.QApplication_Exec()
-}
-
-func echoChanged(echoLineEdit *widgets.QLineEdit, index int) {
-	switch index {
-	case 0:
-		{
-			echoLineEdit.SetEchoMode(widgets.QLineEdit__Normal)
-		}
-
-	case 1:
-		{
-			echoLineEdit.SetEchoMode(widgets.QLineEdit__Password)
-		}
-
-	case 2:
-		{
-			echoLineEdit.SetEchoMode(widgets.QLineEdit__PasswordEchoOnEdit)
-		}
-
-	case 3:
-		{
-			echoLineEdit.SetEchoMode(widgets.QLineEdit__NoEcho)
-		}
-	}
-}
-
-func validatorChanged(validatorLineEdit *widgets.QLineEdit, index int) {
-	switch index {
-	case 0:
-		{
-			validatorLineEdit.SetValidator(nil)
-		}
-
-	case 1:
-		{
-			validatorLineEdit.SetValidator(gui.NewQIntValidator(validatorLineEdit))
-		}
-
-	case 2:
-		{
-			validatorLineEdit.SetValidator(gui.NewQDoubleValidator2(-999.0, 999.0, 2, validatorLineEdit))
-		}
-	}
-
-	validatorLineEdit.Clear()
-}
-
-func alignmentChanged(alignmentLineEdit *widgets.QLineEdit, index int) {
-	switch index {
-	case 0:
-		{
-			alignmentLineEdit.SetAlignment(core.Qt__AlignLeft)
-		}
-
-	case 1:
-		{
-			alignmentLineEdit.SetAlignment(core.Qt__AlignCenter)
-		}
-
-	case 2:
-		{
-			alignmentLineEdit.SetAlignment(core.Qt__AlignRight)
-		}
-	}
-}
-
-func inputMaskChanged(inputMaskLineEdit *widgets.QLineEdit, index int) {
-	switch index {
-	case 0:
-		{
-			inputMaskLineEdit.SetInputMask("")
-		}
-
-	case 1:
-		{
-			inputMaskLineEdit.SetInputMask("+99 99 99 99 99;_")
-		}
-
-	case 2:
-		{
-			inputMaskLineEdit.SetInputMask("0000-00-00")
-			inputMaskLineEdit.SetText("00000000")
-			inputMaskLineEdit.SetCursorPosition(0)
-		}
-
-	case 3:
-		{
-			inputMaskLineEdit.SetInputMask(">AAAAA-AAAAA-AAAAA-AAAAA-AAAAA;#")
-		}
-	}
-}
-
-func accessChanged(accessLineEdit *widgets.QLineEdit, index int) {
-	switch index {
-	case 0:
-		{
-			accessLineEdit.SetReadOnly(false)
-		}
-
-	case 1:
-		{
-			accessLineEdit.SetReadOnly(true)
-		}
-	}
 }
